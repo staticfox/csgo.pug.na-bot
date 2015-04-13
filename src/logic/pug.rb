d8101024fb38057878868129d6639c158c54c59a
Matt Ullman <blindsight@gamesurge.net>
2015-04-13 14:04:47 -0400

    Clean up 'why'

--- src/logic/pug.rb
+++ src/logic/pug.rb
@@ -542,52 +542,33 @@ module PugLogic
 
   def why_logic channel, nick, ourself, ourid
     baddies_id = playerid_by_nick nick
-    if baddies_id != 0
-      is_restricted = $con.query("SELECT irc_players.nick, restrictions.captain, restrictions.time_restricted, restrictions.unrestrict_at, restrictions.reason 
-        FROM restrictions 
-        INNER JOIN irc_players ON restrictions.restrictor_id = irc_players.player_id 
-        WHERE restrictions.player_id = '#{baddies_id}'")
-      if is_restricted.num_rows > 0
-        restrictor = notes = ""
-        captain = time_restricted = unrestrict_at = 0
-        while row = is_restricted.fetch_row do
-          restrictor = row[0].to_s
-          captain = row[1].to_i
-          time_restricted = row[2].to_i
-          unrestrict_at = row[3].to_i
-          notes = row[4].to_s
-        end
-        if captain == 1
-          return pm(channel, "0,1#{nick} was captain restricted by #{restrictor} on #{Time.at(time_restricted)} and will be unrestricted at #{Time.at(unrestrict_at)}. Admin note: #{notes}", 1, nil)
-        else
-          return pm(channel, "0,1#{nick} was restricted by #{restrictor} on #{Time.at(time_restricted)} and will be unrestricted at #{Time.at(unrestrict_at)}. Admin note: #{notes}", 1, nil)
-        end
+    if baddies_id == 0
+      baddies_id = ourid
+      baddiesnick = ourself
+    else
+      baddiesnick = nick
+    end
+    is_restricted = $con.query("SELECT irc_players.nick, restrictions.captain, restrictions.time_restricted, restrictions.unrestrict_at, restrictions.reason 
+      FROM restrictions 
+      INNER JOIN irc_players ON restrictions.restrictor_id = irc_players.player_id 
+      WHERE restrictions.player_id = '#{baddies_id}'")
+    if is_restricted.num_rows > 0
+      restrictor = notes = ""
+      captain = time_restricted = unrestrict_at = 0
+      while row = is_restricted.fetch_row do
+        restrictor = row[0].to_s
+        captain = row[1].to_i
+        time_restricted = row[2].to_i
+        unrestrict_at = row[3].to_i
+        notes = row[4].to_s
+      end
+      if captain == 1
+        return pm(channel, "0,1#{baddiesnick} was captain restricted by #{restrictor} on #{Time.at(time_restricted)} and will be unrestricted at #{Time.at(unrestrict_at)}. Admin note: #{notes}", 1, nil)
       else
-        return pm(channel, "0,1#{nick} is not restricted.", 1, nil)
+        return pm(channel, "0,1#{baddiesnick} was restricted by #{restrictor} on #{Time.at(time_restricted)} and will be unrestricted at #{Time.at(unrestrict_at)}. Admin note: #{notes}", 1, nil)
       end
     else
-      is_restricted = $con.query("SELECT irc_players.nick, restrictions.captain, restrictions.time_restricted, restrictions.unrestrict_at, restrictions.reason 
-        FROM restrictions 
-        INNER JOIN irc_players ON restrictions.restrictor_id = irc_players.player_id 
-        WHERE restrictions.player_id = '#{ourid}'")
-      if is_restricted.num_rows > 0
-        restrictor = notes = ""
-        captain = time_restricted = unrestrict_at = 0
-        while row = is_restricted.fetch_row do
-          restrictor = row[0].to_s
-          captain = row[1].to_i
-          time_restricted = row[2].to_i
-          unrestrict_at = row[3].to_i
-          notes = row[4].to_s
-        end
-        if captain == 1
-          return pm(channel, "0,1#{ourself} was captain restricted by #{restrictor} on #{Time.at(time_restricted)} and will be unrestricted at #{Time.at(unrestrict_at)}. Admin note: #{notes}", 1, nil)
-        else
-          return pm(channel, "0,1#{ourself} was restricted by #{restrictor} on #{Time.at(time_restricted)} and will be unrestricted at #{Time.at(unrestrict_at)}. Admin note: #{notes}", 1, nil)
-        end
-      else
-        return pm(channel, "0,1#{ourself} is not restricted.", 1, nil)
-      end
+      return pm(channel, "0,1#{baddiesnick} is not restricted.", 1, nil)
     end
   end
 
